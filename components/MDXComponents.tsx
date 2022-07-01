d80e0c885dda16c32c80f06a36293abdd58b7483
Tonatiuj Sánchez <tonsanzjimz@gmail.com>
2022-07-01 16:01:53 -0500

    Tipado en MDXComponent resuelto

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -1,6 +1,7 @@
 // Dependencies
 import Link, { LinkProps } from 'next/link'
 import Image from 'next/image'
+import { ReactNode } from 'react';
 
 const CustomLink = (props: React.ComponentProps<"a"> & LinkProps) => {
     const href = props.href;
@@ -17,7 +18,7 @@ const CustomLink = (props: React.ComponentProps<"a"> & LinkProps) => {
     return <a target="_blank" rel="noopener noreferrer" {...props} />;
 };
 
-const MDXComponents = {
+const MDXComponents: any = {
     a: CustomLink,
     Image,
 }
